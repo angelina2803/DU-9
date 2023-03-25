de9639d813fa74183476011908691e384497da65
angelina2803 <108215140+angelina2803@users.noreply.github.com>
2023-03-25 10:18:25 +0100

    Update uko3.js

--- uko3.js
+++ uko3.js
@@ -2,9 +2,9 @@
 const textInput = document.querySelector('#textIn')
 textInput.addEventListener('input', (event) => {
     if(textInput.value === '' || !textInput.value.includes('@')) {
-		textInput.classList.add('.ramecek')
+		textInput.classList.add('ramecek')
 	} else {
-		textInput.classList.remove('.ramecek')
+		textInput.classList.remove('ramecek')
 	}
 })
 
