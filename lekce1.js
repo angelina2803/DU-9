c836a47da1325a441d596c857795ebafe1727456
Angelina Yumatova <>
2023-03-25 16:13:13 +0100

    change

--- lekce1.js
+++ lekce1.js
@@ -1,15 +1,46 @@
-// UKOL1
-document.querySelector('#sipka1').addEventListener('click', () => {
-	const obr = document.querySelector('.auto')
-	obr.style.left = "20px";
- })
-document.querySelector('#sipka2').addEventListener('click', () => {
-    const obr = document.querySelector('.auto')
-    obr.style.right = "20px";
- });
+// // UKOL1
 
+// const obr = document.querySelector('.auto')
+// document.querySelector('#sipka1').addEventListener('click', () => {
+//     obr.style.marginRight = "50px";
+//  })
+//  document.querySelector('#sipka2').addEventListener('click', () => {
+//     obr.style.marginLeft = "50px";
+//  })
+function moveImage(direction) {
 
+	var img = document.getElementById("myImage");
 
+	var currentMarginLeft = parseInt(window.getComputedStyle(img).marginLeft);
+	var currentMarginRight = parseInt(window.getComputedStyle(img).marginRight);
+
+	if (direction === 'left') {
+	  img.style.marginLeft = (currentMarginLeft - 100) + "px";
+	  img.style.marginRight = (currentMarginRight + 100) + "px";
+	} else if (direction === 'right') {
+	  img.style.marginLeft = (currentMarginLeft + 100) + "px";
+	  img.style.marginRight = (currentMarginRight - 100) + "px";
+	}
+  }
+// document.querySelector('#sipka2').addEventListener('click', () => {
+//     const obr = document.querySelector('.auto')
+//     obr.classList.add(posunDoleva);
+//  });
+
+// function moveImageR() {
+// 	var img = document.getElementById(".auto");
+	
+// 	var currentPos = parseInt(img.style.left);
+
+// 	img.style.left = (currentPos + 100) + "px";
+//   }
+//   function moveImageL() {
+// 	var img = document.getElementById(".auto");
+	
+// 	var currentPos = parseInt(img.style.left);
+
+// 	img.style.left = (currentPos + 100) + "px";
+//   }
 //UKOL2
 const user = {
 	email: 'bartolomej.rozumbrada@gmail.com',
